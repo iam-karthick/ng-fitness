b4db8400e750587c622cd62a016056c452fb036c
karthick <30967392+iam-karthick@users.noreply.github.com>
2020-05-28 21:39:51 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCiYYiY-QS2FgmuM0Y2t34fhkszprUaQgQ",
-    authDomain: "ngfitness-app.firebaseapp.com",
-    databaseURL: "https://ngfitness-app.firebaseio.com",
-    projectId: "ngfitness-app",
-    storageBucket: "ngfitness-app.appspot.com",
-    messagingSenderId: "599275815133",
-    // appId: "1:599275815133:web:0c4a58b7f75dbc57"
-  }
+
 };
